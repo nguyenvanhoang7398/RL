150d32e4f370bcfc4d5302b4e70470dc390bff40
Van-Hoang Nguyen <vanguyen@paypal.com>
2020-04-19 11:13:34 +0800

    Update readme and cli

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Reinforcement Learning
+
+## Getting started
+Change your environment config in `parking/env.py`
+
+Train DQN model
+```
+python dqn.py --train
+```
+
+Run reward shaping
+```
+python parking.py --rs
+```
+
+Train DAGGER
+```
+python parking.py --dagger
+```
+
+Test many
+```
+python parking.py --test-many
+```
+
+Test single
+```
+python parking.py --test-single
+```
--- parking.py
+++ parking.py
@@ -1,9 +1,29 @@
 from parking import run_test
 from parking.reward_shaping import heuristic_reward
 from parking.dagger import run_dagger
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Graph Learning')
+    parser.add_argument('--rs', action="store_true", help="Running reward shaping")
+    parser.add_argument('--dagger', action="store_true", help="Running DAGGER")
+    parser.add_argument('--test-many', action="store_true", help="Test on many examples")
+    parser.add_argument('--test-single', action="store_true", help="Test on a single example")
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    # heuristic_reward()
-    # run_test(mode="many")
-    run_dagger()
+    args = parse_args()
+    if args.rs:
+        print("Run reward shaping")
+        heuristic_reward()
+    if args.dagger:
+        print("Run DAGGER")
+        run_dagger()
+    if args.test_many:
+        print("Test many")
+        run_test(mode="many")
+    if args.test_single:
+        print("Test single")
+        run_test(mode="single")
